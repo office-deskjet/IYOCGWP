340c4c375de6cfa4aa7c73f0f76cec17b65c2b24
office-deskjet <office_deskjet@protonmail.com>
2018-04-29 13:49:11 -0700

    tested working, cleaned up code

--- ch3/guess.py
+++ ch3/guess.py
@@ -4,14 +4,14 @@ import random
 START = 1
 STOP = 20
 MAX_GUESSES = 6
-number = random.randint(1, 20)
+number = random.randint(START, STOP)
 
-print("Hello, what is your name?")
+print("Hello, what is your name? ", end="")
 playerName = input()
 print(playerName + ", I am thinking of a number between " + str(START) + " and " + str(STOP))
 
 for i in range(MAX_GUESSES):
-    print("Guess the number: ")
+    print("Guess the number: ", end="")
     guess = input()
     guess = int(guess)
     
