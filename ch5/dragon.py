2e0ed00c2c056439d67249de8e6d79af355e6805
office-deskjet <office_deskjet@protonmail.com>
2018-04-29 15:07:49 -0700

    chooseCave function

--- ch5/dragon.py
+++ ch5/dragon.py
@@ -10,4 +10,11 @@ and will share his treasure with you. The other dragon
 is greedy and hungry, and will eat you on sight.''')
     print()
 
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print("Which cave do you enter? (1 or 2): ", end="")
+        cave = input()
+
+chosenCave = chooseCave()
 displayIntro()
