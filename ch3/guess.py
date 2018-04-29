e05dd9f0ba56cb03c9754a2ce947a78131a26066
office-deskjet <office_deskjet@protonmail.com>
2018-04-29 13:31:45 -0700

    adding more

--- ch3/guess.py
+++ ch3/guess.py
@@ -2,9 +2,16 @@
 import random
 
 guessesTaken = 0
+START = 1
+STOP = 20
+MAX_GUESSES = 6
+number = random.randint(1, 20)
 
 print("Hello, what is your name?")
 playerName = input()
+print(playerName + ", I am thinking of a number between " + str(START) + " and " + str(STOP))
 
-number = random.randint(1, 20)
+for i in range(MAX_GUESSES):
+    print("guess")
+pass
 
