e6dd7cc1649fb9cf4c3e9bd3efdca5b14443adc3
office-deskjet <office_deskjet@protonmail.com>
2018-04-29 13:39:01 -0700

    completed, not tested

--- ch3/guess.py
+++ ch3/guess.py
@@ -1,7 +1,6 @@
 # this is a number guessing game
 import random
 
-guessesTaken = 0
 START = 1
 STOP = 20
 MAX_GUESSES = 6
@@ -12,6 +11,24 @@ playerName = input()
 print(playerName + ", I am thinking of a number between " + str(START) + " and " + str(STOP))
 
 for i in range(MAX_GUESSES):
-    print("guess")
-pass
+    print("Guess the number: ")
+    guess = input()
+    guess = int(guess)
+    
+    if guess < number:
+        print("Your guess is too low.")
+
+    if guess > number:
+        print("Your guess is too high.")
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessTaken = i + 1
+    guessTaken = str(guessTaken)
+    print("Good job " + playerName + "! you guess the number in " + guessTaken + " guesses.")
+else:
+    number = str(number)
+    print("Sorry, the correct number was " + number + ".")
 
