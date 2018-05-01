9b44a07cff6358a120080fcab77215e6e4f6ebe3
office-deskjet <office_deskjet@protonmail.com>
2018-04-30 17:26:52 -0700

    adding files

---
+++ ch6/dragon.py
@@ -0,0 +1,50 @@
+# choose between two caves of dragons, death or treasure!
+import random
+import time
+
+def displayIntro():
+    # ''' means muliline string
+    print('''You are in a land full of dragons. In front of you,
+you see two caves. In one cave, the dragon is friendly
+and will share his treasure with you. The other dragon 
+is greedy and hungry, and will eat you on sight.''')
+    print()
+
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print("Which cave do you enter? (1 or 2): ", end="")
+        cave = input()
+    return cave
+
+def checkCave(chosenCave):
+    print("You approach the cave...")
+    time.sleep(1)
+
+    print("It is dark and spooky...")
+    time.sleep(1)
+
+    print("A large dragon jumps out in front of you! He opens his jaws and..")
+    print()
+    time.sleep(2)
+
+    # returns an int
+    friendlyCave = random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+        print("Gives you his treasure!")
+    else:
+        print("Gobels you up in one bite!")
+
+    
+playAgain = "yes"
+while playAgain == "yes" or playAgain == "y":
+    displayIntro()
+    chosenCave = chooseCave()
+    checkCave(chosenCave)
+
+    print()
+    print("do you want to play again? (yes or no): ", end="")
+    playAgain = input()
+
+print("Thanks for playing, bye.")
