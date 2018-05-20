e1fb86a1f68b1dba51e9b6755788aac5b38879e7
office-deskjet <office_deskjet@protonmail.com>
2018-05-19 22:35:13 -0700

    added and tested drawBoard, note index 0 ignored!

---
+++ ch10/tictactoe.py
@@ -0,0 +1,13 @@
+# Tic-Tac-Toe with an AI
+
+import random
+
+# Board looks like number pad. We ignore index 0
+def drawBoard(board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
+
